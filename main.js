11142520c7c59063c6fcc30a53003ac07df43184
Yago Gomes <yagogomesc@gmail.com>
2022-01-05 21:25:41 -0300

    Update

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const { app, BrowserWindow, ipcMain } = require("electron");
 const windowStateKeeper = require("electron-window-state");
 const readItem = require("./readItem");
 const appMenu = require("./menu");
+const updater = require("./updater");
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -21,6 +22,7 @@ ipcMain.on("new-item", (e, itemUrl) => {
 
 // Create a new BrowserWindow when `app` is ready
 function createWindow() {
+  setTimeout(updater, 3000);
   // Win state keeper
   let state = windowStateKeeper({
     defaultWidth: 500,
---
+++ updater.js
@@ -0,0 +1,39 @@
+const { dialog } = require("electron");
+const { autoUpdater } = require("electron-updater");
+
+autoUpdater.logger = require("electron-log");
+autoUpdater.logger.transports.file.level = "info";
+
+autoUpdater.autoDownload = false;
+
+module.exports = () => {
+  autoUpdater.checkForUpdates();
+
+  autoUpdater.on("update-available", () => {
+    dialog
+      .showMessageBox({
+        type: "info",
+        title: "Update available",
+        message: "New updated available",
+        buttons: ["Update", "No"],
+      })
+      .then((result) => {
+        let buttonIndex = result.response;
+
+        if (buttonIndex === 0) autoUpdater.downloadUpdate();
+      });
+  });
+
+  autoUpdater.on("update-downloaded", () => {
+    dialog({
+      type: "info",
+      title: "Update ready",
+      message: "Install and restart now?",
+      buttons: ["Yes", "Later"],
+    }).then((result) => {
+      let buttonIndex = result.response;
+
+      if (buttonIndex === 0) autoUpdater.quitAndInstall(false, true);
+    });
+  });
+};
